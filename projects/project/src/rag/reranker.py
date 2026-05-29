5d6b61ffbfe75c5abf54cc2276219ab4c9597947
joao-vitor-rodrigues-ey <joao.vitor.rodrigues@br.ey.com>
2026-05-29 10:20:50 -0300

    fix(rag): restaura reranker

---
+++ projects/project/src/rag/reranker.py
@@ -0,0 +1,17 @@
+import re
+from typing import List
+
+def simple_rerank(query: str, chunks: List[dict]) -> List[dict]:
+    """Re-rankeia os chunks por relevância baseado em palavras-chave em comum com a query."""
+    
+    query_words = set(re.findall(r'\w+', query.lower()))
+    
+    reranked = []
+    for chunk in chunks:
+        content_words = set(re.findall(r'\w+', chunk['content'].lower()))
+        common_words = query_words.intersection(content_words)
+        new_chunk = dict(chunk)
+        new_chunk['rerank_score'] = len(common_words)
+        reranked.append(new_chunk)
+    
+    return sorted(reranked, key=lambda x: x['rerank_score'], reverse=True)
